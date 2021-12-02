ed4c00e9de97c95c8513ec776b2374d43e26eb33
Caden Kroonenberg <cskroonenberg@gmail.com>
2021-12-02 14:10:27 -0600

    Add import protection and include new data set-up

--- tweeter.py
+++ tweeter.py
@@ -2,76 +2,89 @@
 # Date: 11-29-21
 
 import csv
-import keys
+import data.info as info
 import tweepy
+import os
 
-# Init twitter user
-CONSUMER_KEY = keys.CONSUMER_KEY
-CONSUMER_SECRET = keys.CONSUMER_SECRET
-ACCESS_KEY = keys.ACCESS_KEY
-ACCESS_SECRET = keys.ACCESS_SECRET
-BEARER_TOKEN = keys.BEARER_TOKEN
+def fetch_val(file_name):
+    if not os.path.isfile(file_name):
+        raise FileNotFoundError
+    # get date
+    file = open(file_name, "r+")
+    val = file.read()
+    file.close()
+    return val
 
-# Your username
-username = 'og_ron_c_'
+def overwrite(file_name, val):
+    if not os.path.isfile(file_name):
+        raise FileNotFoundError
+    # Overwrite
+    file = open(file_name, "r+")
+    file.seek(0)
+    file.write(str(val))
+    file.truncate()
+    file.close()
+    return val
 
-# CSV file where actions are stored (see sample_actions.csv)
-actions_url = "actions.csv"
+if __name__ == "__main__":
+    # Init twitter user
+    CONSUMER_KEY = info.CONSUMER_KEY
+    CONSUMER_SECRET = info.CONSUMER_SECRET
+    ACCESS_KEY = info.ACCESS_KEY
+    ACCESS_SECRET = info.ACCESS_SECRET
+    BEARER_TOKEN = info.BEARER_TOKEN
 
-# Thing you're quitting
-addiction_str = 'nicotine'
+    # Your username
+    username = info.USERNAME
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth)
-client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
+    # CSV file where actions are stored (see sample_actions.csv)
+    actions_url = "tweets.csv"
 
-user = client.get_user(username=username)[0]
+    # Thing you're quitting
+    addiction_str = info.ADDICTION
 
-id = user.id
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+    api = tweepy.API(auth)
+    client = tweepy.Client(BEARER_TOKEN, CONSUMER_KEY, CONSUMER_SECRET, ACCESS_KEY, ACCESS_SECRET)
 
-# get date
-day_file = open(r"day.txt", "r+")
-day = int(day_file.read()) + 1
+    user = client.get_user(username=username)[0]
+    id = user.id
 
-qrt_file = open(r"qrt.txt", "r+")
-to_quote_id = qrt_file.read()
+    # Fetch day and QRT values
+    day = int(fetch_val("data/day.txt")) + 1
+    qrt_id = int(fetch_val("data/qrt.txt"))
 
-# reading csv file
-with open(actions_url, 'r') as csvfile:
-    actions = []
-    # creating a csv reader object
-    csvreader = csv.reader(csvfile)
-  
-    # extracting each data row one by one
-    for row in csvreader:
-        actions.append(row[0])
-  
-    # get total number of actions
-    if csvreader.line_num < day:
-        print("Need new content!")
+    # reading csv file
+    with open(actions_url, 'r') as csvfile:
+        actions = []
+        # creating a csv reader object
+        csvreader = csv.reader(csvfile)
+    
+        # extracting each data row one by one
+        for row in csvreader:
+            actions.append(row[0])
+    
+        # get total number of actions
+        if csvreader.line_num < day:
+            print("Need new content!")
 
-action = str(actions[day])
+    action = str(actions[day])
 
-# Create tweet string
-to_tweet = "Day " + str(day) + " no " + addiction_str + ": " + action
+    # Create tweet string
+    to_tweet = "Day " + str(day) + " no " + addiction_str + ": " + action
 
-# Tweet day and action
-print("Tweeting \"" + to_tweet + "\"")
-client.create_tweet(text=to_tweet)
-print("Tweet sent!")
+    # Tweet day and action
+    print("Tweeting \"" + to_tweet + "\"")
+    if qrt_id == 0:
+        client.create_tweet(text=to_tweet)
+    else:
+        client.create_tweet(text=to_tweet, quote_tweet_id=qrt_id)
+    print("Tweet sent!")
 
-# Overwrite date in day file
-day_file.seek(0)
-day_file.write(str(day))
-day_file.truncate()
-day_file.close()
+    most_recent_tweet = client.get_users_tweets(id, max_results=5)[0][0]
+    most_recent_id = most_recent_tweet.id
 
-most_recent_tweet = client.get_users_tweets(id, max_results=1)[0][0]
-most_recent_id = most_recent_tweet.id
-
-# Overwrite qrt ID in qrt file
-qrt_file.seek(0)
-qrt_file.write(str(most_recent_id)) # TODO: Update ID
-qrt_file.truncate()
-qrt_file.close()
+    # Set day and QRT values for next script execution
+    overwrite("data/qrt.txt", most_recent_id)
+    overwrite("data/day.txt", day)

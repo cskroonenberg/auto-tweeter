acc6081ca631f8897c237cc8e023fc3ea3130ce8
Caden Kroonenberg <cskroonenberg@gmail.com>
2022-01-08 15:02:57 -0600

    Add initialization print

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 import schedule
 import time
-
+import data.info as info
 # Execute bot.py
 def tweet():
     print(time.ctime())
-    exec(open("bot.py").read())
-    
+    exec(open("tweeter.py").read())
+
 if __name__ == "__main__":
     # Execute interval (every day at 12 PM)
     schedule.every().day.at("12:00").do(tweet)
     print(time.ctime())
+    print('Initializing \'' + info.USERNAME + '\' bot.')
     while 1:
         schedule.run_pending()
         time.sleep(1)
